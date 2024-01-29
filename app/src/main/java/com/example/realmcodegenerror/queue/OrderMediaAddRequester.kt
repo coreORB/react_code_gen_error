1bc23c4ef20f27a70b38fa4afb8c28590819ee18
coreORB <rafal.ziemlanka@gmail.com>
2024-01-29 15:02:03 +0100

    error on OrderMediaAddRequester.io_realm_kotlin_schema() after adding equals(), hashCode() and toString().

--- app/src/main/java/com/example/realmcodegenerror/queue/OrderMediaAddRequester.kt
+++ app/src/main/java/com/example/realmcodegenerror/queue/OrderMediaAddRequester.kt
@@ -16,4 +16,15 @@ class OrderMediaAddRequester(
     OrderMediaRealmObject::class,
     listener
 ) {
+    override fun equals(other: Any?): Boolean {
+        return super.equals(other)
+    }
+
+    override fun hashCode(): Int {
+        return super.hashCode()
+    }
+
+    override fun toString(): String {
+        return super.toString()
+    }
 }
